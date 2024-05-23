7533b6bd2630b8aca4dca504d3b63e6b4b4eb104
Ybanez921 <gabrielybanez921@gmail.com>
2024-05-22 22:24:42 -0400

    helppp

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -82,14 +82,15 @@ def index():
     if request.method == 'GET':
 
         # Get current values
-        # Get current values
         vote1 = r.get(button1).decode('utf-8')
-        with tracer.span(name="Cats Vote") as span:
-           print("Cats Vote")
-        # tracer.span(name="Cats Vote")
+        #with tracer.span(name="Cats Vote") as span:
+         #   print("Cats Vote")
+        tracer.span(name="Cats Vote")
         vote2 = r.get(button2).decode('utf-8')
-        with tracer.span(name="Dogs Vote") as span:
-           print("Dogs Vote")
+        #with tracer.span(name="Dogs Vote") as span:
+         #   print("Dogs Vote")
+         
+        tracer.span(name="Dogs Vote")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
